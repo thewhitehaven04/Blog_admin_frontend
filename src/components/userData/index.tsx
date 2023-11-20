e582ab8e4e2f1479180a53aa18114e487147cdf8
German <thewhitehaven04@gmail.com>
2023-11-20 23:30:34 +0100

    header styling changes

--- src/components/userData/index.tsx
+++ src/components/userData/index.tsx
@@ -4,10 +4,10 @@ import { type IUserDataProps } from 'Components/userData/types'
 
 // placeholder
 export const UserData = (props: IUserDataProps): JSX.Element => (
-  <CardWrapper>
-    <Column>
-      <span>{props.username}</span>
-      <span>{props.email}</span>
-    </Column>
-  </CardWrapper>
+  <Column $alignment='end'>
+    <CardWrapper>
+      <div>{props.username}</div>
+      <div>{props.email}</div>
+    </CardWrapper>
+  </Column>
 )
